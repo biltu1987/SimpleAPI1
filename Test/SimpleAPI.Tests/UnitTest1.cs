45538054320e5515b22b4ebff35aaa2a96db7f11
biltu1987 <biltu.khawas@gmail.com>
2021-10-25 22:25:44 +0530

    Phase1

---
+++ Test/SimpleAPI.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Tests
+{
+    public class UnitTest1
+    {
+        WeatherForecastController obj;
+        [Fact]
+        public void GetReturnValue()
+        {
+            obj = new WeatherForecastController();
+            var _result = obj.Get(1);
+            Assert.Equal("Biltu Khawas",_result.Value);
+        }
+    }
+}
